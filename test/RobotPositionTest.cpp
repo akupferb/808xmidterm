79ea138cbafc4d940b79be1bf366c21a190cd9dd
lydiazoghbi <lalzogbi@terpmail.umd.edu>
2019-10-14 12:25:41 -0400

    Task 1.14 - Adding license preample to all .cpp and .hpp files associated with headers, and adding cpplint and cppcheck results

--- test/RobotPositionTest.cpp
+++ test/RobotPositionTest.cpp
@@ -1,3 +1,18 @@
+/*
+ *  Distributed under the Boost Software License.
+ *  Version 1.0 (See accompanying file LICENSE_1_0.txt
+ *  or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+/**
+ *  @file       RobotPositionTest.cpp
+ *  @author     Lydia Zoghbi
+ *  @copyright  Copyright 2019 ARL. All rights reserved as per license.
+ *  @date       10/13/2019
+ *  @version    1.0
+ *
+ *  @brief      Unit test
+ *
+ */
 #include <gtest/gtest.h>
 #include "RobotPosition.hpp"
 
--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,18 @@
+/*
+ *  Distributed under the Boost Software License.
+ *  Version 1.0 (See accompanying file LICENSE_1_0.txt
+ *  or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+/**
+ *  @file       main.cpp
+ *  @author     Lydia Zoghbi
+ *  @copyright  Copyright 2019 ARL. All rights reserved as per license.
+ *  @date       10/13/2019
+ *  @version    1.0
+ *
+ *  @brief      Runs all unit tests
+ *
+ */
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
