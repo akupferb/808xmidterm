52726d6b823a0b5f7bab041704eb3d2a4c4a059b
rjb3vp <rjb3vp@virginia.edu>
2019-10-17 16:58:46 -0400

    Task 2.1- Created stub for RobotTest

---
+++ test/RobotTest.cpp
@@ -0,0 +1,23 @@
+/*
+ *  Distributed under the Boost Software License.
+ *  Version 1.0 (See accompanying file LICENSE_1_0.txt
+ *  or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+/**
+ *  @file       RobotTest.cpp
+ *  @author     Ryan Bates
+ *  @copyright  Copyright 2019 ARL. All rights reserved as per license.
+ *  @date       10/17/2019
+ *  @version    1.0
+ *
+ *  @brief      Unit test
+ *
+ */
+#include <gtest/gtest.h>
+#include "Robot.hpp"
+
+TEST(RobotTest, Constructor) {
+
+   
+}
+
