faa3b7816ef347c61df14a59b7029fe4c04249ab
rjb3vp <rjb3vp@umd.edu>
2019-10-21 04:32:39 -0400

    Task 1.06 [system_test_demo.sh] Tiny edits/comments

--- system_test_demo.sh
+++ system_test_demo.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 ##
 ##  Distributed under our modified Boost Software License.
 ##  Version 1.0 (see accompanying file LICENSE)
@@ -13,9 +15,16 @@
 ##
 
 
-#!/bin/sh
+
 #         executable [start][---end position--] [-----obstacle 1--rad] [-----obstacle 2--rad]
 ./build/app/shell-app 0 0 0 2050.0 30.0 -1200.0 100000.1, 0.2, 0.3 0.5 100000.5, 0.6, 0.7 1.0
+
+# Expects a succesfully planned path!
+
+
+
+
+
 
 
 # Other tests for the curious:
@@ -37,3 +46,8 @@
 # ./build/app/shell-app 0 0 0 2050.0 30.0 -1200.0
 
 
+
+
+# Interpretation of the original 'system test' defined in the original SYSTEM_TEST_GUIDELINE.txt
+
+# ./build/app/shell-app 0 0 0 2.5000 0 -0.5800
